72ed828d1e74d1c49ff8356c084f45a26eed3834
ID-4149\founder4149 <zeng_kai@housei-inc.com>
2022-06-29 11:55:33 +0900

    边栏二级子菜单跳转功能编辑中

--- src/components/sandbox/SideMenu.tsx
+++ src/components/sandbox/SideMenu.tsx
@@ -7,76 +7,91 @@ import {
   UserOutlined,
 } from "@ant-design/icons";
 import "./index.css";
+import { useNavigate } from "react-router-dom";
+import Item from "antd/lib/list/Item";
+import { icons } from "antd/lib/image/PreviewGroup";
 const { Sider } = Layout;
 
-//模拟数组结构
-// const items = [
-//   {
-//     key: "/home",
-//     icon: <HomeOutlined />,
-//     label: "ホーム",
-//   },
-//   {
-//     key: "/user-manage/list",
-//     icon: <TeamOutlined />,
-//     label: "カスタマイズ",
-//     children: [
-//       {
-//         key: "/right-manage/role/list",
-//         label: "権限管理",
-//         icon: <TeamOutlined />,
-//       },
-//       {
-//         key: "/right-manage/right/list",
-//         label: "スペース管理",
-//         icon: <TeamOutlined />,
-//       },
-//     ],
-//   },
-//   {
-//     key: "3",
-//     icon: <UserOutlined />,
-//     label: "ユーザー",
-//     children: [
-//       {
-//         key: "3-1",
-//         label: "test",
-//         icon: <TeamOutlined />,
-//       },
-//     ],
-//   },
-//   {
-//     key: "4",
-//     icon: <AreaChartOutlined />,
-//     label: "事件管理",
-//     children: [
-//       {
-//         key: "4-1",
-//         label: "アップロード申請",
-//         icon: <TeamOutlined />,
-//       },
-//       {
-//         key: "4-1",
-//         label: "空間申請",
-//         icon: <TeamOutlined />,
-//       },
-//     ],
-//   },
-//   {
-//     key: "5",
-//     icon: <AreaChartOutlined />,
-//     label: "レポート",
-//     children: [
-//       {
-//         key: "5-1",
-//         label: "test",
-//         icon: <TeamOutlined />,
-//       },
-//     ],
-//   },
-// ];
+//模拟数组结构,后期取于后台
+const items = [
+  {
+    key: "/home",
+    icon: <HomeOutlined />,
+    label: "ホーム",
+  },
+  {
+    key: "/user-manage/list",
+    icon: <TeamOutlined />,
+    label: "カスタマイズ",
+    children: [
+      {
+        key: "/right-manage/role/list",
+        label: "権限管理",
+        icon: <TeamOutlined />,
+      },
+      {
+        key: "/right-manage/right/list",
+        label: "スペース管理",
+        icon: <TeamOutlined />,
+      },
+    ],
+  },
+  // {
+  //   key: "3",
+  //   icon: <UserOutlined />,
+  //   label: "ユーザー",
+  //   children: [
+  //     {
+  //       key: "3-1",
+  //       label: "test",
+  //       icon: <TeamOutlined />,
+  //     },
+  //   ],
+  // },
+  // {
+  //   key: "4",
+  //   icon: <AreaChartOutlined />,
+  //   label: "事件管理",
+  //   children: [
+  //     {
+  //       key: "4-1",
+  //       label: "アップロード申請",
+  //       icon: <TeamOutlined />,
+  //     },
+  //     {
+  //       key: "4-1",
+  //       label: "空間申請",
+  //       icon: <TeamOutlined />,
+  //     },
+  //   ],
+  // },
+  // {
+  //   key: "5",
+  //   icon: <AreaChartOutlined />,
+  //   label: "レポート",
+  //   children: [
+  //     {
+  //       key: "5-1",
+  //       label: "test",
+  //       icon: <TeamOutlined />,
+  //     },
+  //   ],
+  // },
+];
 
-export default function SideMenu() {
+export default function SideMenu(props) {
+  useNavigate();
+  const renderMenu = (
+    items: {
+      key: string;
+      icon: JSX.Element;
+      label: string;
+      children?: { key: string; label: string; icon: JSX.Element }[];
+    }[]
+  ) => {
+    return { items };
+  };
+
   return (
     <Sider trigger={null} collapsible collapsed={false}>
       <div className="logo">Tera Box</div>
@@ -84,71 +99,10 @@ export default function SideMenu() {
         theme="dark"
         mode="inline"
         defaultSelectedKeys={["1"]}
-        items={[
-          {
-            key: "1",
-            icon: <HomeOutlined />,
-            label: "ホーム",
-          },
-          {
-            key: "2",
-            icon: <TeamOutlined />,
-            label: "カスタマイズ",
-            children: [
-              {
-                key: "2-1",
-                label: "権限管理",
-                icon: <TeamOutlined />,
-              },
-              {
-                key: "2-2",
-                label: "スペース管理",
-                icon: <TeamOutlined />,
-              },
-            ],
-          },
-          {
-            key: "3",
-            icon: <UserOutlined />,
-            label: "ユーザー",
-            children: [
-              {
-                key: "3-1",
-                label: "test",
-                icon: <TeamOutlined />,
-              },
-            ],
-          },
-          {
-            key: "4",
-            icon: <AreaChartOutlined />,
-            label: "事件管理",
-            children: [
-              {
-                key: "4-1",
-                label: "アップロード申請",
-                icon: <TeamOutlined />,
-              },
-              {
-                key: "4-1",
-                label: "空間申請",
-                icon: <TeamOutlined />,
-              },
-            ],
-          },
-          {
-            key: "5",
-            icon: <AreaChartOutlined />,
-            label: "レポート",
-            children: [
-              {
-                key: "5-1",
-                label: "test",
-                icon: <TeamOutlined />,
-              },
-            ],
-          },
-        ]}
+        {...renderMenu(items)}
+        onClick={() => {
+          props.history.push(items.keys);
+        }}
       />
     </Sider>
   );
