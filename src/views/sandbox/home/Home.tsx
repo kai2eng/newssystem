2620d36233e870b91709a5ae53798b8a13047efc
ID-4149\founder4149 <zeng_kai@housei-inc.com>
2022-06-30 14:54:27 +0900

    add json serber

--- src/views/sandbox/home/Home.tsx
+++ src/views/sandbox/home/Home.tsx
@@ -1,5 +1,36 @@
+import axios from "axios";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  const ajax = () => {
+    //取数据
+    // axios.get("http://localhost:5010/posts").then((res) => {
+    //   console.log(res.data);
+    // });
+    //增加数据
+    // axios.post("http://localhost:5010/posts", {
+    //   id: "1333",
+    //   title: "json123rver",
+    //   author: "typ222ode",
+    // });
+    // //修改数据
+    // axios.patch("http://localhost:5010/posts/1", {
+    //   title: "zeng",
+    //   sex: "ssss",
+    // });
+    // //删除
+    // axios.delete("http://localhost:5010/posts/2");
+    //向下连接_embed
+    axios.get("http://localhost:5010/posts?_embed=comments").then((res) => {
+      console.log(res.data);
+    });
+    //向上查找_expand
+  };
+  return (
+    <div>
+      <button type="submit" onClick={ajax}>
+        Home
+      </button>
+    </div>
+  );
 }
